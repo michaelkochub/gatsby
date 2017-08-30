ca2f3c8268801a32a64281a6ba9be4b318eb19b9
michaelkochub <michaelkochub@gmail.com>
2017-08-30 19:24:46 -0400

    the script works 🙂

--- gatsby-proj/src/pages/index.js
+++ gatsby-proj/src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+export default () => 
+	<div style={{ color: `tomato` }}>
+		<h1>Hello Koch 🎉</h1>
+		<h2>Testing out the build...</h2>
+		<p>What a day...</p>
+		<img src="http://lorempixel.com/400/200/" />
+		<br />
+		<Link to="/page-2/">Link</Link>
+		<br />
+		<Link to="/counter/">counter</Link>
+	</div>
