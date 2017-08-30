4b7d0fa9fc07eecd8aacec8ce38b61badf97ee39
michaelkochub <michaelkochub@gmail.com>
2017-08-30 19:16:20 -0400

    back to basics

--- gatsby-proj/src/pages/index.js
+++ gatsby-proj/src/pages/index.js
@@ -1,14 +1,3 @@
 import React from "react"
-import Link from "gatsby-link"
 
-export default () => 
-	<div style={{ color: `tomato` }}>
-		<h1>Hello Koch 🎉</h1>
-		<h2>REALLY Testing out the build...</h2>
-		<p>What a day...</p>
-		<img src="http://lorempixel.com/400/200/" />
-		<br />
-		<Link to="/page-2/">Link</Link>
-		<br />
-		<Link to="/counter/">counter</Link>
-	</div>
+export default () => <div>Hello world!</div>
--- gatsby-proj/src/pages/page-2.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import Link from "gatsby-link"
-
-export default () => 
-	<div style={{ color: `tomato` }}>
-		<h1>Page 2 😳</h1>
-		<Link to="/">back home</Link>
-	</div>
