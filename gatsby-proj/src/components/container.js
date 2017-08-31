9ca0cf5c57b8f03988aa5136fb0c7bcf913913db
michaelkochub <michaelkochub@gmail.com>
2017-08-31 13:07:27 -0400

    part two typography.js and css modules

---
+++ gatsby-proj/src/components/container.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default ({ children }) =>
+  <div style={{ margin: "3rem auto", maxWidth: 600 }}>
+  	<div style={{ padding: "0 3rem 0" }} >
+    	{children}
+    </div>
+  </div>
--- gatsby-proj/src/pages/index.js
+++ gatsby-proj/src/pages/index.js
@@ -1,14 +1,18 @@
 import React from "react"
 import Link from "gatsby-link"
 
+import Container from "../components/container"
+
 export default () => 
-	<div style={{ color: `tomato` }}>
-		<h1>Hello Koch 🎉</h1>
-		<h2>REALLY Testing out the build...</h2>
-		<p>What a day...</p>
-		<img src="http://lorempixel.com/400/200/" />
-		<br />
-		<Link to="/page-2/">Link</Link>
-		<br />
-		<Link to="/counter/">counter</Link>
-	</div>
+    <Container>
+        <h1>Hello Koch 🎉</h1>
+        <h2>REALLY Testing out the build...</h2>
+        <p>What a day...</p>
+        <img src="http://lorempixel.com/400/200/" />
+        <br />
+        <Link to="/page-2/">Link</Link>
+        <br />
+        <Link to="/counter/">Counter</Link>
+        <br />
+        <Link to="/about-css-modules/">About css modules</Link>
+    </Container>
