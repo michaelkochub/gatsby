bc3f98b9de466abefba0595bde0582fd6cd9ea83
michaelkochub <michaelkochub@gmail.com>
2017-09-01 18:35:36 -0400

    part 3 of tutorial - layout components

--- gatsby-proj/src/pages/index.js
+++ gatsby-proj/src/pages/index.js
@@ -1,18 +1,10 @@
-import React from "react"
-import Link from "gatsby-link"
+import React from 'react'
 
-import Container from "../components/container"
-
-export default () => 
-    <Container>
-        <h1>Hello Koch 🎉</h1>
-        <h2>REALLY Testing out the build...</h2>
-        <p>What a day...</p>
-        <img src="http://lorempixel.com/400/200/" />
-        <br />
-        <Link to="/page-2/">Link</Link>
-        <br />
-        <Link to="/counter/">Counter</Link>
-        <br />
-        <Link to="/about-css-modules/">About css modules</Link>
-    </Container>
+export default () =>
+  <div>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </div>
